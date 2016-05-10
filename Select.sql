4ef25b07f9634ea3af454ff3096d8ea4d9342167
Redas <Incaras@gmail.com>
2016-05-10 11:39:54 +0300

    Fix grammar, Add exercise

--- Select.sql
+++ Select.sql
@@ -1,4 +1,4 @@
-/*
+﻿/*
 PARDAVIMAI
 Veikla   Miestas     Pardavimai  Suma
 Veikla1 Klaipėda         200              1600
@@ -8,7 +8,7 @@ Veikla2 Vilnius          200              2000
 Veikla3 Vilnius          555              666
 */
 
---Kaip pasirinkti pardavimus, veiklą tik iš kauno? ATS:
+--Kaip pasirinkti pardavimus, veiklą tik iš Kauno? ATS:
 Select Pardavimai, Veikla From Pardavimai Where Miestas = N'Kaunas'
 
 --Kaip atsirinkti pardavimus iš miestų, prasidedančių tik su K raide? ATS: 
@@ -17,10 +17,10 @@ Select Pardavimai From Pardavimai Where Miestas Like 'K%'
 --Kaip sudėti pardavimus iš Kauno ir Klaipėdos? ATS:
  Select Sum(Pardavimai) From Pardavimai Where Miestas = N'Kaunas' Or Miestas = N'Klaipėda'
 
---[Sugalvotas] Kiek unikalių veiklų turi kiekvienas miestas? ATS: 
+--Kiek unikalių veiklų turi kiekvienas miestas? ATS: 
 Select Miestas, Count(Distinct Veikla) From Pardavimai Group By Miestas
 
---[Sugalvotas] Parodyti pardavimus didėjimo tvarką, o sumą mažėjimo tvarka. ATS: 
+--Parodyti pardavimus didėjimo tvarką, o sumą mažėjimo tvarka. ATS: 
 Select * From Pardavimai Order By Pardavimai
 
 Select * From Pardavimai Order By Suma Desc
