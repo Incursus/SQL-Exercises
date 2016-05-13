36df0c50ad4ede380db263011327bb451b0d6e79
Incursus <redas.kriksciukaitis@gmail.com>
2016-05-13 18:35:10 +0300

    Update and rename BasicSQLCommands.sql to Crud.sql

---
+++ Crud.sql
@@ -0,0 +1,15 @@
+--1. Įterpti 3 naujus įrašus. (C-create)
+Insert into Friend (Id, Name)
+Values (1, 'Shawn')
+Insert into Friend (Id, Name)
+Values (2, 'Ad')
+Insert into Friend (Id, Name)
+Values (3, 'Milda')
+
+--2. Gauti visus duomenis. (R-read)
+Select * From Friend
+
+--3. Pakeisti vieno iš įterptų įrašų Name reikšmę. (U-update) [TO DO]
+
+--4. Ištrinti visus lentelėje esančius įrašus. (D-delete)
+Delete From Friend
