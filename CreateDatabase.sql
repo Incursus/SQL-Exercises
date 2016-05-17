468f1d1521ceaf3e42894b37395a2b99be128451
Redas <Incaras@gmail.com>
2016-05-17 19:53:15 +0300

    Update and complete exercise

--- CreateDatabase.sql
+++ CreateDatabase.sql
@@ -1,4 +1,4 @@
---Su SQL komandom susikurt duomenø bazæ Test ir po to jà iðtrinti.
+--Su SQL komandom susikurt duomenø bazæ Test ir po to jà iðtrinti. Prieð kuriant patikrinti ar duomenø bazë egzistuoja. Jei neegzistuoja - nekurti (if sàlyga) bet dropinti. 
 
 Create Database Test
 Drop Database Test
--- TableJoins.sql
+++ TableJoins.sql
@@ -2,7 +2,9 @@ use [People]
 
 --1. Paraðyti Join, kuris gauna visus Streamer vardus ir Game pavadinimus, kurie sutampa. T.y. rodyti tik tuos streamerius, kurie loðia sutampanèius þaidimus.
 
-Select PrimaryGameId, Streamer.Name
+Select Streamer.Name, Game.Name
 From Streamer
 Inner Join Game
 On Streamer.PrimaryGameId=Game.Id
+
+--2. Paraðyti Join, kuris gauna Streamer vardus ir Game pavadinimus ið visø Streamer áraðø ir sutampanèiø Game áraðø.
